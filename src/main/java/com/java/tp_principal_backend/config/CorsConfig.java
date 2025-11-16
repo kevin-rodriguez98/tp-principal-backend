118275916ab6479ec26a264f8a56eb4724b1c901
Aldana Micaela <aldana.filiberto17@gmail.com>
2025-11-15 22:25:29 -0300

    modifique el Cors

--- src/main/java/com/java/tp_principal_backend/config/CorsConfig.java
+++ src/main/java/com/java/tp_principal_backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // todas las rutas
-                        .allowedOrigins("https://tp-principal-frontend.netlify.app") // dominio permitido
+                        .allowedOrigins("https://tp-principal-frontend.netlify.app", "http://localhost:5173/") // dominio permitido
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                        .allowedHeaders("*")
                         .allowCredentials(true);
