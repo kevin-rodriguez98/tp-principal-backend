d3c38964bcec391346c4fe27aea1b3c6f4470442
kevin rodriguez <kevinleonel.rodriguez.contractor@gdevtools.com>
2025-11-17 13:22:37 -0300

    modifico test que rompen

--- src/test/java/com/java/tp_principal_backend/service/OrdenProduccionServiceTest.java
+++ src/test/java/com/java/tp_principal_backend/service/OrdenProduccionServiceTest.java
@@ -2,6 +2,8 @@ package com.java.tp_principal_backend.service;
 
 import com.java.tp_principal_backend.data.OrdenProduccionDao;
 import com.java.tp_principal_backend.data.ProductosDao;
+import com.java.tp_principal_backend.dto.EtapaRequest;
+import com.java.tp_principal_backend.dto.OrdenFinalizadaRequest;
 import com.java.tp_principal_backend.dto.OrdenProduccionRequest;
 import com.java.tp_principal_backend.model.MovimientoProducto;
 import com.java.tp_principal_backend.model.OrdenProduccion;
@@ -97,8 +99,11 @@ public class OrdenProduccionServiceTest {
         when(productosDao.findByCodigo("P1")).thenReturn(Optional.of(producto));
         when(movimientoProductoService.restarInsumos(producto, orden.getStockRequerido())).thenReturn(true);
         when(ordenDao.save(any(OrdenProduccion.class))).thenAnswer(invocation -> invocation.getArgument(0));
-
-        OrdenProduccion result = ordenService.marcarEnProduccion(1, "P1");
+        EtapaRequest etapa = new EtapaRequest();
+        etapa.setEstado("EN_PRODUCCION");
+        etapa.setIdOrden(1);
+        etapa.setLegajo("100");
+        OrdenProduccion result = ordenService.actualizarEtapa(etapa);
 
         assertEquals("EN_PRODUCCION", result.getEstado());
         assertEquals("P1", result.getCodigoProducto());
@@ -113,8 +118,11 @@ public class OrdenProduccionServiceTest {
 
         when(ordenDao.findById(1)).thenReturn(Optional.of(orden));
         when(ordenDao.save(any(OrdenProduccion.class))).thenAnswer(invocation -> invocation.getArgument(0));
-
-        OrdenProduccion result = ordenService.marcarCancelada(1);
+        EtapaRequest etapa = new EtapaRequest();
+        etapa.setEstado("EVALUACION");
+        etapa.setIdOrden(1);
+        etapa.setLegajo("100");
+        OrdenProduccion result = ordenService.actualizarEtapa(etapa);
 
         assertEquals("CANCELADA", result.getEstado());
     }
@@ -123,11 +131,14 @@ public class OrdenProduccionServiceTest {
     void testMarcarCancelada_Fail() {
         OrdenProduccion orden = new OrdenProduccion();
         orden.setId(1);
-        orden.setEstado("EN_PRODUCCION");
-
+        orden.setEstado("CANCELDA");
+        EtapaRequest etapa = new EtapaRequest();
+        etapa.setEstado("CANCELDA");
+        etapa.setIdOrden(1);
+        etapa.setLegajo("100");
         when(ordenDao.findById(1)).thenReturn(Optional.of(orden));
 
-        assertThrows(IllegalStateException.class, () -> ordenService.marcarCancelada(1));
+        assertThrows(IllegalStateException.class, () -> ordenService.actualizarEtapa(etapa));
     }
 
     @Test
@@ -150,8 +161,12 @@ public class OrdenProduccionServiceTest {
 
         // Se espera que se llame al egreso automático
         doReturn(new MovimientoProducto()).when(movimientoProductoService).egresoAutomatico("P1", BigDecimal.valueOf(10), destino);
-
-        OrdenProduccion result = ordenService.marcarFinalizada(1, BigDecimal.valueOf(12), destino);
+        OrdenFinalizadaRequest ordenFinalizada = new OrdenFinalizadaRequest();
+        ordenFinalizada.setDestino(destino);
+        ordenFinalizada.setLegajo("100");
+        ordenFinalizada.setOrdenId(12);
+        ordenFinalizada.setStockProducidoReal(BigDecimal.valueOf(12));
+        OrdenProduccion result = ordenService.marcarFinalizada(ordenFinalizada);
 
         assertEquals("FINALIZADA_ENTREGADA", result.getEstado());
         assertEquals("LoteABC", result.getLote());
@@ -180,8 +195,12 @@ public class OrdenProduccionServiceTest {
         when(ordenDao.save(any(OrdenProduccion.class))).thenAnswer(invocation -> invocation.getArgument(0));
 
         doReturn(new MovimientoProducto()).when(movimientoProductoService).egresoAutomatico("P1", BigDecimal.valueOf(10), destino);
-
-        OrdenProduccion result = ordenService.marcarFinalizada(1, null, destino);
+        OrdenFinalizadaRequest ordenFinalizada = new OrdenFinalizadaRequest();
+        ordenFinalizada.setDestino(destino);
+        ordenFinalizada.setLegajo("100");
+        ordenFinalizada.setOrdenId(12);
+        ordenFinalizada.setStockProducidoReal(BigDecimal.valueOf(12));
+        OrdenProduccion result = ordenService.marcarFinalizada(ordenFinalizada);
 
         assertEquals("FINALIZADA_ENTREGADA", result.getEstado());
         assertEquals("LoteABC", result.getLote());
@@ -195,10 +214,14 @@ public class OrdenProduccionServiceTest {
         OrdenProduccion orden = new OrdenProduccion();
         orden.setId(1);
         orden.setEstado("EVALUACION");
-
+        String destino = "Cliente1";
         when(ordenDao.findById(1)).thenReturn(Optional.of(orden));
-
-        assertThrows(IllegalStateException.class, () -> ordenService.marcarFinalizada(1, BigDecimal.valueOf(5), "Cliente1"));
+        OrdenFinalizadaRequest ordenFinalizada = new OrdenFinalizadaRequest();
+        ordenFinalizada.setDestino(destino);
+        ordenFinalizada.setLegajo("100");
+        ordenFinalizada.setOrdenId(12);
+        ordenFinalizada.setStockProducidoReal(BigDecimal.valueOf(12));
+        assertThrows(IllegalStateException.class, () -> ordenService.marcarFinalizada(ordenFinalizada));
 
         // Se asegura de que no se llamó al egreso automático
         verify(movimientoProductoService, never()).egresoAutomatico(anyString(), any(), anyString());
